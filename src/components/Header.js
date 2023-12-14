afe27ae758f1b71c96add7a3a2712fceac27f258
Samuel Faioli <scarf1999@gmail.com>
2023-12-14 16:49:34 -0300

    change project

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header-wraper'>
+        <div className='main-info'>
+            <h1>some text</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
