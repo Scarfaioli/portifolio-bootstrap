bf7f5219d9abb7b75ebb2e4393d935a76c9a0bd2
Samuel Faioli <scarf1999@gmail.com>
2023-12-14 17:53:38 -0300

    nav bar e novo app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
 import Navbar from './components/Navbar';
-import Header from './components/Header';
+
 
 function App() {
   return (
-    <h1>Vamos denovo</h1>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/projects' element={<Projects />}></Route>
+          <Route path='/experience' element={<Experience />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
